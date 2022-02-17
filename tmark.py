8b9b487e81fee21d5c559f57ef42877b70536a3a
Muna 'Adilah <93821740+munaadilah@users.noreply.github.com>
2022-02-17 22:32:03 +0700

    Add files via upload

---
+++ tmark.py
@@ -0,0 +1,10 @@
+print("Menentukan Median")
+print("")
+print("[4, 6, 5, 8, 10]")
+
+n = range(5)
+
+sum = 0
+for i in n:
+    sum += 1
+print("Mediannya adalah", sum)
---
+++ tmarkk.py
@@ -0,0 +1,12 @@
+print("Menentukan Median")
+print("")
+print("[4, 6, 5, 8, 10, 12]")
+data = [4, 6, 5, 8, 10, 12]
+
+n = range(5 + 8)
+
+sum = 0
+for i in n:
+    sum += 1
+    a = sum / 2
+print("Mediannya adalah", a)
